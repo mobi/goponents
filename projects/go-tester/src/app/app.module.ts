f967e288a6231475021340592ba1952b3ba05319
Alex Overbeck <alexoverbeck@gmail.com>
2019-05-06 15:30:05 -0400

    Adds code to the go-tester app.component to test off canvas
    This implements a couple of examples in the app.component inside
    the go-tester project. These changes will show how to implement
    the service and give us a visual way to test the components.

--- projects/go-tester/src/app/app.module.ts
+++ projects/go-tester/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { GoButtonModule, GoIconModule, GoTableModule } from '../../../go-lib/src/public_api';
-
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 
-import { GoOffCanvasModule } from 'projects/go-lib/src/lib/components/go-off-canvas/go-off-canvas.module';
+import { 
+  GoButtonModule,
+  GoIconModule,
+  GoOffCanvasService,
+  GoOffCanvasModule,
+  GoTableModule,
+  GoButtonComponent,
+  GoIconComponent
+} from '../../../go-lib/src/public_api';
 
 @NgModule({
   declarations: [
@@ -21,7 +27,14 @@ import { GoOffCanvasModule } from 'projects/go-lib/src/lib/components/go-off-can
     GoTableModule,
     GoOffCanvasModule
   ],
-  providers: [AppService],
+  providers: [
+    AppService,
+    GoOffCanvasService
+  ],
+  entryComponents: [
+    GoButtonComponent,
+    GoIconComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
