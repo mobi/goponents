ace87e35cc2fe8ba985289af3a634d2bf81252c8
Steven Ulmer <steven.ulmer@tangoe.com>
2019-07-10 14:31:44 -0400

    Adds module for a copy button

---
+++ projects/go-lib/src/lib/components/go-copy/go-copy.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  animations: [],
+  selector: 'go-copy',
+  templateUrl: './go-copy.component.html',
+  styleUrls: ['./go-copy.component.scss']
+})
+export class GoCopyComponent {
+  @Input() text: string;
+
+  constructor() { }
+
+  copyStringToClipboard (element: HTMLInputElement): void {
+    (element.children[1] as HTMLInputElement).select();
+    document.execCommand('copy');
+  }
+}
---
+++ projects/go-lib/src/lib/components/go-copy/go-copy.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GoCopyComponent } from './go-copy.component';
+import { GoIconModule } from '../go-icon/go-icon.module';
+
+@NgModule({
+  declarations: [
+    GoCopyComponent
+  ],
+  imports: [
+    CommonModule,
+    GoIconModule
+  ],
+  exports: [
+    GoCopyComponent
+  ]
+})
+
+export class GoCopyModule { }
