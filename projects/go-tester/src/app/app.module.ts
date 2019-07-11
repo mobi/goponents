83c5f2d5bc1df0db79249e8afd635d843683d24b
Steven Ulmer <steven.ulmer@tangoe.com>
2019-07-11 14:23:16 -0400

    Adds a badge component for for buttons and icons

--- projects/go-tester/src/app/app.module.ts
+++ projects/go-tester/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   GoAccordionModule,
   GoActionSheetModule,
+  GoBadgeModule,
   GoButtonComponent,
   GoButtonModule,
   GoCardModule,
@@ -61,6 +62,7 @@ import { AppGuard } from './app.guard';
     ReactiveFormsModule,
     GoAccordionModule,
     GoActionSheetModule,
+    GoBadgeModule,
     GoButtonModule,
     GoCardModule,
     GoCheckboxModule,
