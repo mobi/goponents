a95e37261928120f3838a99cba3db88cf0161901
jaredami <jaredrstorm@gmail.com>
2019-07-24 12:00:10 -0400

    Add title attribute to go-icon-button

--- projects/go-lib/src/lib/components/go-icon-button/go-icon-button.component.ts
+++ projects/go-lib/src/lib/components/go-icon-button/go-icon-button.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class GoIconButtonComponent {
   @Input() buttonDisabled: boolean;
   @Input() buttonIcon: string;
+  @Input() buttonTitle: string;
 
   @Output() handleClick = new EventEmitter();
 
