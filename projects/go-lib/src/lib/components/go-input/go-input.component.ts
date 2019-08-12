f1b9f1611434d9766c9062d4bf6a102f9824d182
Steven Ulmer <steven.ulmer@tangoe.com>
2019-08-12 11:09:15 -0400

    adds ability to toggle disabled and dark theme for input

--- projects/go-lib/src/lib/components/go-input/go-input.component.ts
+++ projects/go-lib/src/lib/components/go-input/go-input.component.ts
@@ -1,22 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'go-input',
   templateUrl: './go-input.component.html'
 })
-export class GoInputComponent {
+export class GoInputComponent implements OnInit {
 
-  @Input() label: string;
-  @Input() inputType: string = 'text';
+  @Input() controlName: string;
   @Input() errors: string[];
   @Input() errorStatus: string = 'Error:';
   @Input() hints: string[];
-  @Input() controlName: string;
+  @Input() inputDisabled: boolean;
+  @Input() inputType: string = 'text';
+  @Input() label: string;
   @Input() parentFormGroup: FormGroup;
   @Input() placeholder: string = '';
+  @Input() theme: string = 'light';
 
   constructor() { }
+
+  ngOnInit(): void {
+    if (!this.controlName) {
+      throw new Error('GoInput: controlName is a required Input');
+    }
+    if (!this.parentFormGroup) {
+      throw new Error('GoInput: parentFormGroup is a required Input');
+    }
+  }
 
   controlIsValid(): boolean {
     return !this.parentFormGroup.get(this.controlName).invalid;
