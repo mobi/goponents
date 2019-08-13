198108d92184af3ac2058441fccc73de98e79cba
Graham Hency <grahamhency@gmail.com>
2019-08-13 15:31:43 -0400

    Fix tests

--- projects/go-lib/src/lib/components/go-table/go-table.component.spec.ts
+++ projects/go-lib/src/lib/components/go-table/go-table.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GoTableComponent } from './go-table.component';
 import { GoIconModule } from '../go-icon/go-icon.module';
 import { GoLoaderModule } from '../go-loader/go-loader.module';
-import { GoTableDataSource } from './go-table-config.model';
+import { GoTableDataSource, GoTableConfig } from './go-table-config.model';
 import { GoTablePageConfig } from './go-table-paging.model';
 
 describe('GoTableComponent', () => {
@@ -24,15 +24,10 @@ describe('GoTableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GoTableComponent);
     component = fixture.componentInstance;
-    component.tableConfig = {
-      dataMode: GoTableDataSource.client,
-      pageConfig: new GoTablePageConfig(),
+    component.tableConfig = new GoTableConfig({
       pageable: true,
-      totalCount: 10,
-      noDataText: '',
-      sortable: true,
       tableData: []
-    };
+    });
     fixture.detectChanges();
   });
 
