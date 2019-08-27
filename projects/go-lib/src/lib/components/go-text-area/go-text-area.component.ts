cd8322cf3f8a3d7cdd3e35212299ff54c9c1962e
Alex Overbeck <AlexOverbeck@users.noreply.github.com>
2019-08-27 13:42:06 -0400

    Update textarea (#144)
    * Update the text-area component to match the other inputs
    
    * Update go-tester to include a text-area component
    
    * Adds basic tests around the text-area component
    
    This tests the basic component functionality, but doesn't test some
    of the side effects that state changes have on the template.

--- projects/go-lib/src/lib/components/go-text-area/go-text-area.component.ts
+++ projects/go-lib/src/lib/components/go-text-area/go-text-area.component.ts
@@ -1,25 +1,33 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'go-text-area',
   templateUrl: './go-text-area.component.html'
 })
-export class GoTextAreaComponent {
+export class GoTextAreaComponent implements OnInit {
+  id: string;
 
-  @Input() controlName: string;
-  @Input() errors: string[];
-  @Input() errorStatus: string = 'Error:';
-  @Input() hints: string[];
-  @Input() inputDisabled: boolean;
+  @Input() control: FormControl;
+  @Input() key: string;
+  @Input() hints: Array<string>;
   @Input() label: string;
-  @Input() parentFormGroup: FormGroup;
   @Input() placeholder: string = '';
-  @Input() theme: string = 'light';
+  @Input() theme: 'light' | 'dark' = 'light';
 
   constructor() { }
 
-  controlIsValid(): boolean {
-    return !this.parentFormGroup.get(this.controlName).invalid;
+  ngOnInit(): void {
+    this.id = this.key || this.generateId(this.label);
+  }
+
+  private generateId(label: string): string {
+    const labelText: string = label || 'text-area';
+    const idArray: Array<string> = labelText.split(' ');
+
+    // NOTE: There is only a one in a million chance that this number is not unique.
+    idArray.push(String(Math.round(Math.random() * 1000000)));
+
+    return idArray.join('-');
   }
 }
--- projects/go-lib/src/lib/components/go-text-area/go-text-area.module.ts
+++ projects/go-lib/src/lib/components/go-text-area/go-text-area.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { GoTextAreaComponent } from './go-text-area.component';
+import { GoHintModule } from '../go-hint/go-hint.module';
 
 @NgModule({
   declarations: [GoTextAreaComponent],
   imports: [
     CommonModule,
     FormsModule,
+    GoHintModule,
     ReactiveFormsModule
   ],
   exports: [GoTextAreaComponent]
