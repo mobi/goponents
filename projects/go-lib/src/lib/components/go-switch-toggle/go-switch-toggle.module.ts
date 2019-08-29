64c4578e22f3132d89d0467ed56c72cfb91c904f
Graham Hency <grahamhency@gmail.com>
2019-08-29 14:55:27 -0400

    Updated css to meet UX needs

--- projects/go-lib/src/lib/components/go-switch-toggle/go-switch-toggle.module.ts
+++ projects/go-lib/src/lib/components/go-switch-toggle/go-switch-toggle.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { GoHintModule } from '../go-hint/go-hint.module';
+
 import { GoSwitchToggleComponent } from './go-switch-toggle.component';
 
 @NgModule({
@@ -9,6 +11,7 @@ import { GoSwitchToggleComponent } from './go-switch-toggle.component';
   imports: [
     CommonModule,
     FormsModule,
+    GoHintModule,
     ReactiveFormsModule
   ],
   exports: [GoSwitchToggleComponent]
