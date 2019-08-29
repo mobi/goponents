8f0e68681ed77a96af78a6126923cf4cca1dd5cd
Alex Overbeck <alexoverbeck@gmail.com>
2019-08-29 15:34:53 -0400

    Updates toggle to have a unique id

--- projects/go-lib/src/lib/components/go-switch-toggle/go-switch-toggle.component.ts
+++ projects/go-lib/src/lib/components/go-switch-toggle/go-switch-toggle.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -6,15 +6,32 @@ import { FormControl } from '@angular/forms';
   templateUrl: './go-switch-toggle.component.html',
   styleUrls: ['./go-switch-toggle.component.scss']
 })
-export class GoSwitchToggleComponent {
+export class GoSwitchToggleComponent implements OnInit {
+  id: string;
+
   @Input() control: FormControl;
+  @Input() key: string;
   @Input() hints: string[];
   @Input() label: string;
-  @Input() theme: string = 'light';
+  @Input() theme: 'light' | 'dark' = 'light';
 
   constructor() { }
 
+  ngOnInit(): void {
+    this.id = this.key || this.generateId(this.label);
+  }
+
   toggle(): void {
     this.control.setValue(!this.control.value);
+  }
+
+  private generateId(label: string): string {
+    const labelText: string = label || 'toggle';
+    const idArray: Array<string> = labelText.split(' ');
+
+    // NOTE: There is only a one in a million chance that this number is not unique.
+    idArray.push(String(Math.round(Math.random() * 1000000)));
+
+    return idArray.join('-');
   }
 }
