c54d7cf575521aef3731a92fa1f856a4643745b4
Steven Ulmer <steven.ulmer@tangoe.com>
2019-08-30 15:02:03 -0400

    update badge styles

--- projects/go-lib/src/lib/components/go-badge/go-badge.component.ts
+++ projects/go-lib/src/lib/components/go-badge/go-badge.component.ts
@@ -1,16 +1,22 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, HostBinding, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'go-badge',
   templateUrl: './go-badge.component.html',
   styleUrls: ['./go-badge.component.scss']
 })
-export class GoBadgeComponent {
+export class GoBadgeComponent implements OnChanges {
+  badgeStyles: object;
+
   @Input() badgeData: string;
   @Input() badgeColor: string = 'neutral';
   @Input() displayData: boolean = true;
 
   @HostBinding('class.go-badge__parent')
+
+  ngOnChanges(): void {
+    this.badgeStyles = this.badgeClasses();
+  }
 
   badgeClasses(): object {
     return {
