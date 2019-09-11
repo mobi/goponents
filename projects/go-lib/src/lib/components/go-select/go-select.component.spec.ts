63d9a97d2c4d36441286da8eb25aedadc54d7b22
Steven Ulmer <steven.ulmer@tangoe.com>
2019-09-11 11:29:26 -0400

    Add a label for the go select component

--- projects/go-lib/src/lib/components/go-select/go-select.component.spec.ts
+++ projects/go-lib/src/lib/components/go-select/go-select.component.spec.ts
@@ -34,4 +34,41 @@ describe('GoSelectComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('ngOnInit()', () => {
+    beforeEach(() => {
+      component.id = undefined;
+    });
+
+    it('sets the id of the input to `key` if one is passed in', () => {
+      expect(component.id).toBeUndefined();
+
+      component.key = 'a-specific-id';
+      component.ngOnInit();
+
+      expect(component.id).toBe(component.key);
+    });
+
+    it('generates a semi-random id based on the label if key is undefined', () => {
+      expect(component.key).toBeUndefined();
+      expect(component.id).toBeUndefined();
+
+      component.label = 'test label';
+      component.ngOnInit();
+
+      expect(component.id).toBeDefined();
+      expect(component.id).toContain('test-label-');
+    });
+
+    it('generates a semi-random id if a label and key are undefined', () => {
+      expect(component.key).toBeUndefined();
+      expect(component.label).toBeUndefined();
+      expect(component.id).toBeUndefined();
+
+      component.ngOnInit();
+
+      expect(component.id).toBeDefined();
+      expect(component.id).toContain('select-');
+    });
+  });
 });
--- projects/go-lib/src/lib/components/go-select/go-select.component.ts
+++ projects/go-lib/src/lib/components/go-select/go-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,30 @@ import { FormControl } from '@angular/forms';
   templateUrl: './go-select.component.html',
   styleUrls: ['./go-select.component.scss']
 })
-export class GoSelectComponent {
+export class GoSelectComponent implements OnInit {
+  id: string;
+
   @Input() bindLabel: string;
   @Input() bindValue: string;
   @Input() control: FormControl;
   @Input() hints: string[];
   @Input() items: any[];
+  @Input() key: string;
+  @Input() label: string;
   @Input() multiple: boolean = false;
   @Input() theme: 'light';
+
+  ngOnInit(): void {
+    this.id = this.key || this.generateId(this.label);
+  }
+
+  private generateId(label: string): string {
+    const labelText: string = label || 'select';
+    const idArray: Array<string> = labelText.split(' ');
+
+    // NOTE: There is only a one in a million chance that this number is not unique.
+    idArray.push(String(Math.round(Math.random() * 1000000)));
+
+    return idArray.join('-');
+  }
 }
