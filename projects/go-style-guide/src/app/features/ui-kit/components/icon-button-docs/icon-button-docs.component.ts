f2c458232bbf735516138c99d9cfb1db2b085cb1
Jared Storm <jaredstorm@Jareds-MacBook-Pro.local>
2019-10-14 10:53:53 -0400

    Remove unused scss file

--- projects/go-style-guide/src/app/features/ui-kit/components/icon-button-docs/icon-button-docs.component.ts
+++ projects/go-style-guide/src/app/features/ui-kit/components/icon-button-docs/icon-button-docs.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-icon-button-docs',
-  templateUrl: './icon-button-docs.component.html',
-  styleUrls: ['./icon-button-docs.component.scss']
+  templateUrl: './icon-button-docs.component.html'
 })
 export class IconButtonDocsComponent {
   pageTitle: string = 'Icon Button';
