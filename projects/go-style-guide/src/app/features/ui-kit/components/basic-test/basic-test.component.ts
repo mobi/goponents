a945c59df3ae99a524b7447fc800402a9d59a37f
Steven Ulmer <steven.ulmer@tangoe.com>
2019-12-06 11:47:12 -0500

    Updates inputs in style guide off canvas to use go-input instead of just input classes

--- projects/go-style-guide/src/app/features/ui-kit/components/basic-test/basic-test.component.ts
+++ projects/go-style-guide/src/app/features/ui-kit/components/basic-test/basic-test.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import * as faker from 'faker';
 import { GoButtonComponent, GoOffCanvasService } from '../../../../../../../go-lib/src/public_api';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-test',
@@ -10,9 +11,13 @@ import { GoButtonComponent, GoOffCanvasService } from '../../../../../../../go-l
 export class BasicTestComponent {
   @ViewChild('submitButton') submitButton: GoButtonComponent;
 
+  email: FormControl = new FormControl('');
   fakeTitle: string = faker.company.companyName();
   fakeCompanyBs: string = faker.company.bs();
   fakePhrase: string = faker.company.catchPhrase();
+  firstName: FormControl = new FormControl('');
+  lastName: FormControl = new FormControl('');
+  password: FormControl = new FormControl('');
 
   constructor(
     private goOffCanvasService: GoOffCanvasService
