934a44afcf6da0fd77665e439997f77aa475ce87
Steven Ulmer <steven.ulmer@tangoe.com>
2019-12-18 10:46:11 -0500

    fix typo in copy doc tests

--- projects/go-style-guide/src/app/features/ui-kit/components/copy-docs/copy-docs.component.spec.ts
+++ projects/go-style-guide/src/app/features/ui-kit/components/copy-docs/copy-docs.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CopyDocsComponent } from './copy-docs.component';
 
-describe('ButtonDocsComponent', () => {
+describe('CopyDocsComponent', () => {
   let component: CopyDocsComponent;
   let fixture: ComponentFixture<CopyDocsComponent>;
 
