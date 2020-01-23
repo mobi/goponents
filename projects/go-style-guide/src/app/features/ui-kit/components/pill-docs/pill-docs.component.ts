5ee35a94893033b42804fb3f303558789c90ff55
Eliezer.Golding <eliezer.golding@tangoe.com>
2020-01-23 17:45:04 -0500

    add examples to pills docs

--- projects/go-style-guide/src/app/features/ui-kit/components/pill-docs/pill-docs.component.ts
+++ projects/go-style-guide/src/app/features/ui-kit/components/pill-docs/pill-docs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pill-docs',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 export class PillDocsComponent {
   pageTitle: string = 'Filter Pills';
 
+  showPill: boolean = true;
+
   componentBindings: string = `
   @Input() removable: boolean = true;
   @Output() removed: EventEmitter<void> = new EventEmitter();
   `;
 
+  basic_html: string = `
+  <go-pill [removable]="false">Without remove icon</go-pill>
+  `;
+  dismiss_html: string = `
+  <go-pill (removed)="removed()">With remove icon</go-pill>
+  `;
+
+  dismiss_ts: string = `
+  showPill = true;
+
+  removed() {
+    this.showPill = false;
+  }
+  `;
+
+  removed(): void {
+    this.showPill = false;
+  }
 }
