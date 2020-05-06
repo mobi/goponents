e5d6f51d7351804a47957795c9eb765dbd86900e
Eliezer.Golding <eliezer.golding@tangoe.com>
2020-05-06 14:19:51 -0400

    failing test for undefined and null in table row blowing up when extracting field data

--- projects/go-lib/src/lib/components/go-table/go-table-utils.spec.ts
+++ projects/go-lib/src/lib/components/go-table/go-table-utils.spec.ts
@@ -1,4 +1,4 @@
-import { sortBy } from './go-table-utils';
+import { sortBy, extractFieldData } from './go-table-utils';
 
 describe('go-table-utils', () => {
   describe('sortBy', () => {
@@ -19,6 +19,23 @@ describe('go-table-utils', () => {
       ];
       const result = orig.sort(sortBy('last', true));
       expect(result[0].first).toBe('Hagrid');
+    });
+  });
+  describe('extractFieldData', () => {
+    it('should treat undefined as empty string', () => {
+      const row = {
+        first: 'Voldemort',
+      };
+      const result: string = extractFieldData('last', row)
+      expect(result).toBe('');
+    });
+    it('should treat null as empty string', () => {
+      const row = {
+        first: 'Voldemort',
+        last: null
+      };
+      const result: string = extractFieldData('last', row)
+      expect(result).toBe('');
     });
   });
 });
