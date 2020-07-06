95460e55ffcba84544df6930f32c0d9f06800893
Jared Storm <jaredrstorm@gmail.com>
2020-07-06 14:16:22 -0400

    Make GoCopyDocLink directive more generic and publicly accessible (#607)
    This commit renames the the GoCopyDocLinkDirective to GoCopyCardLinkDirective, adds the directive to our
    public api, and adds documentation for how to use it to our style guide.

--- projects/go-style-guide/src/app/features/standards/standards.module.ts
+++ projects/go-style-guide/src/app/features/standards/standards.module.ts
@@ -6,6 +6,7 @@ import { StandardsRoutesModule } from './routes/standards-routing.module';
 
 import { GoCardModule } from '../../../../../go-lib/src/public_api';
 import { GoCopyModule } from '../../../../../go-lib/src/public_api';
+import { GoCopyCardLinkModule } from 'projects/go-lib/src/lib/directives/go-copy-card-link/go-copy-card-link.module';
 import { GoButtonModule } from '../../../../../go-lib/src/public_api';
 
 // Module Components
@@ -21,6 +22,7 @@ import { TypographyComponent } from './components/typography/typography.componen
     StandardsRoutesModule,
     GoCardModule,
     GoCopyModule,
+    GoCopyCardLinkModule,
     GoButtonModule
   ],
   declarations: [
