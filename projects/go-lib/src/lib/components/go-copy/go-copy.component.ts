164d2cf0e7eb2d933a5a878ea0647864277d4319
Jared Storm <jaredrstorm@gmail.com>
2020-07-09 15:01:47 -0400

    Add fade animation

--- projects/go-lib/src/lib/components/go-copy/go-copy.component.ts
+++ projects/go-lib/src/lib/components/go-copy/go-copy.component.ts
@@ -1,7 +1,35 @@
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component, ElementRef, HostBinding, Input, ViewChild } from '@angular/core';
 
 @Component({
-  animations: [],
+  animations: [
+    trigger('checkMarkFadeInOut', [
+      state('content_copy', style({
+        opacity: '1'
+      })),
+      state('check', style({
+        opacity: '1'
+      })),
+      transition('content_copy => check', [
+        group([
+          animate('3s', keyframes([
+            style({ opacity: '0', offset: 0 }),
+            style({ opacity: '1', offset: 0.1 }),
+            style({ opacity: '1', offset: 0.9 }),
+            style({ opacity: '0', offset: 1 })
+          ]))
+        ])
+      ]),
+      transition('check => content_copy', [
+        group([
+          animate('300ms', keyframes([
+            style({ opacity: '0' }),
+            style({ opacity: '1' })
+          ]))
+        ])
+      ]),
+    ])
+  ],
   selector: 'go-copy',
   templateUrl: './go-copy.component.html',
   styleUrls: ['./go-copy.component.scss']
@@ -23,6 +51,6 @@ export class GoCopyComponent {
     this.icon = 'check';
     setTimeout(() => {
       this.icon = 'content_copy';
-    }, 2000);
+    }, 3000);
   }
 }
