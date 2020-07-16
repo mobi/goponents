b3ffbed76098f960f35f01d90154735876985272
Jared Storm <jaredrstorm@gmail.com>
2020-07-16 14:36:54 -0400

    Add header component unit tests

--- projects/go-lib/src/lib/components/go-header/go-header.component.spec.ts
+++ projects/go-lib/src/lib/components/go-header/go-header.component.spec.ts
@@ -1,15 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { GoHeaderComponent } from './go-header.component';
-import { GoIconModule } from '../go-icon/go-icon.module';
 import { CommonModule } from '@angular/common';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { skip } from 'rxjs/operators';
+import { BrandingMode, GoConfigInterface } from '../../go-config.model';
 import { GoConfigService } from '../../go-config.service';
+import { GoIconModule } from '../go-icon/go-icon.module';
+import { GoHeaderComponent } from './go-header.component';
 
 describe('GoHeaderComponent', () => {
   let component: GoHeaderComponent;
   let fixture: ComponentFixture<GoHeaderComponent>;
+  let configService: GoConfigService;
+
+  const configMock: GoConfigInterface = {
+    brandColor: '#ffffff',
+    brandingMode: BrandingMode.company,
+    logoConfig: {
+      logo: 'hedwig.jpg'
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -30,10 +40,27 @@ describe('GoHeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GoHeaderComponent);
     component = fixture.componentInstance;
+    configService = TestBed.get(GoConfigService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('setupConfig', () => {
+    it('subscribes to configuration subject of configService', () => {
+      expect(component.brandColor).toBe('#65B360');
+      expect(component.logoConfig).toBe(undefined);
+
+      configService.config.pipe(skip(1)).subscribe(() => {
+        expect(component.brandColor).toBe('#ffffff');
+        expect(component.logoConfig).toEqual({ logo: 'hedwig.jpg' });
+        expect(component.menuBgHoverValue).toBe('#e6e6e6');
+        expect(component.fontColor).toEqual('#ffffff');
+      });
+
+      configService.setConfig(configMock);
+    });
   });
 });
