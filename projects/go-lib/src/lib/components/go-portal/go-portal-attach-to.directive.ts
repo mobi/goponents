92ff0e0be8508c83900f337307d337b62721cf9e
Eliezer.Golding <eliezer.golding@tangoe.com>
2020-08-11 12:06:33 -0400

    go-portal module

---
+++ projects/go-lib/src/lib/components/go-portal/go-portal-attach-to.directive.ts
@@ -0,0 +1,25 @@
+import {Directive, Input, OnDestroy, OnInit, TemplateRef} from '@angular/core';
+import {GoPortalService} from './go-portal.service';
+
+@Directive({
+  selector: '[goPortalAttachTo]'
+})
+export class GoPortalAttachToDirective implements OnInit, OnDestroy{
+  @Input('goPortalAttachTo') targetName: string;
+
+  constructor(
+      private portalService: GoPortalService,
+      private template: TemplateRef<any>
+  ) {}
+
+  ngOnInit(): void {
+    this.portalService.attachToTarget(this.targetName, this.template);
+  }
+
+  ngOnDestroy(): void {
+    this.portalService.clearTarget(this.targetName);
+  }
+
+
+
+}
---
+++ projects/go-lib/src/lib/components/go-portal/go-portal.service.ts
@@ -0,0 +1,27 @@
+import {Injectable, TemplateRef, ViewContainerRef} from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class GoPortalService {
+  private targets: Map<string, ViewContainerRef>;
+
+  constructor() {
+    this.targets = new Map<string, ViewContainerRef>();
+  }
+
+  addTarget(targetName: string, viewContainer: ViewContainerRef): void {
+    this.targets.set(targetName, viewContainer);
+  }
+
+  attachToTarget(targetName: string, template: TemplateRef<any>): void {
+    this.getTarget(targetName).createEmbeddedView(template);
+  }
+
+  clearTarget(targetName: string): void {
+    this.getTarget(targetName).clear();
+  }
+
+  private getTarget(targetName: string): ViewContainerRef | null {
+    return this.targets.has(targetName) ? this.targets.get(targetName) : null;
+  }
+
+}
