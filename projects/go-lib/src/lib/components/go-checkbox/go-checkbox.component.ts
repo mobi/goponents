de6c0f7d979c7e10e5ac20ae1399cfc090f0e5a4
Jared Storm <jaredrstorm@gmail.com>
2020-10-23 10:52:15 -0400

    Update go-checkbox background color and integrate into go-table. (#670)

--- projects/go-lib/src/lib/components/go-checkbox/go-checkbox.component.ts
+++ projects/go-lib/src/lib/components/go-checkbox/go-checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { generateId } from '../../utilities/form.utils';
 
@@ -6,18 +6,35 @@ import { generateId } from '../../utilities/form.utils';
   selector: 'go-checkbox',
   templateUrl: './go-checkbox.component.html'
 })
-export class GoCheckboxComponent implements OnInit {
+export class GoCheckboxComponent implements OnInit, OnChanges, AfterViewInit {
   id: string;
 
   @Input() control: FormControl;
   @Input() hints: string[];
+  @Input() indeterminate: boolean = false;
   @Input() key: string;
   @Input() label: string;
   @Input() theme: string = 'light';
+
+  @ViewChild('hiddenInputRef', { static: false }) hiddenInputRef: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
     this.id = this.key || generateId(this.label, 'checkbox');
+  }
+
+  ngOnChanges(): void {
+    if (this.hiddenInputRef) {
+      this.setIndeterminateState();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.setIndeterminateState();
+  }
+
+  private setIndeterminateState(): void {
+    this.hiddenInputRef.nativeElement.indeterminate = this.indeterminate;
   }
 }
