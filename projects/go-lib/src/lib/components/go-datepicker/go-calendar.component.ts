547a449f0167f70bcb2d3b4d3e9c7c8b66b665c0
Amit Patra <amit.patra@tangoe.com>
2020-11-27 02:31:47 +0530

    Update code

--- projects/go-lib/src/lib/components/go-datepicker/go-calendar.component.ts
+++ projects/go-lib/src/lib/components/go-datepicker/go-calendar.component.ts
@@ -140,7 +140,7 @@ export class GoCalendarComponent implements OnDestroy, OnInit {
           visibility: 'visible',
         });
         document.body.appendChild(calenderBody);
-      }, 100);
+      });
     }
   }
 }
