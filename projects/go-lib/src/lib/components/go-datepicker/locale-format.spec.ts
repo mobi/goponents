189cfa7d08720421852f40181f56d39a5155c9ed
jay.kravetz@tangoe.com <jay.kravetz@tangoe.com>
2021-08-30 10:45:31 -0400

    Remove @see annotation

--- projects/go-lib/src/lib/components/go-datepicker/locale-format.spec.ts
+++ projects/go-lib/src/lib/components/go-datepicker/locale-format.spec.ts
@@ -2,9 +2,6 @@ import {LocaleFormat} from './locale-format';
 
 describe('LocaleFormat', () => {
   describe('validDate()', () => {
-    /**
-     * @see https://github.com/mobi/goponents/issues/828
-     */
     it('should validate dates between jan & dec', () => {
       const jan: Date = new Date(2020, 0, 1);
       const dec: Date = new Date(2020, 11, 1);
@@ -20,14 +17,13 @@ describe('LocaleFormat', () => {
     /**
      * @see https://github.com/mobi/goponents/issues/828
      *
+     * it('should return false when dates are out of range', () => {
+     *   const badDate: Date = new Date(2021, -1, 32);
      *
-     *    it('should return false when dates are out of range', () => {
-     *      const badDate: Date = new Date(2021, -1, 32);
-     *
-     *      expect(LocaleFormat.validDate(badDate.getMonth(), badDate.getDate(), badDate.getFullYear())).toBeFalsy(
-     *        'Date(2020, -1, 32) should be invalid'
-     *      );
-     *    });
+     *   expect(LocaleFormat.validDate(badDate.getMonth(), badDate.getDate(), badDate.getFullYear())).toBeFalsy(
+     *     'Date(2020, -1, 32) should be invalid'
+     *   );
+     * });
      */
   });
 });
