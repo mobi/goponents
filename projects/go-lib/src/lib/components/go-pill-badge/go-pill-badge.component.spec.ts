55b72a74e29e44ed90ea068cddbd03358e960c08
Saravanan Muralidharan <saravanan.m@tangoe.com>
2023-03-02 17:39:10 +0530

    code coverage

--- projects/go-lib/src/lib/components/go-pill-badge/go-pill-badge.component.spec.ts
+++ projects/go-lib/src/lib/components/go-pill-badge/go-pill-badge.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { GoPillBadgeComponent } from './go-pill-badge.component';
+import { GoPillBadgeComponent } from "./go-pill-badge.component";
 
-describe('GoPillBadgeComponent', () => {
+describe("GoPillBadgeComponent", () => {
   let component: GoPillBadgeComponent;
   let fixture: ComponentFixture<GoPillBadgeComponent>;
 
@@ -18,7 +18,37 @@ describe('GoPillBadgeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  describe("badgeClasses()", () => {
+    beforeEach(() => {
+      component.badgeStyles = {};
+    });
+
+    it("returns an object that sets go-pill-badge--success to true based on type", () => {
+      expect(component.badgeStyles["go-pill-badge--success"]).toBeFalsy();
+
+      component.type = "success";
+      component.ngOnChanges();
+      expect(component.badgeStyles["go-pill-badge--success"]).toBe(true);
+    });
+
+    it("returns an object that set go-pill-badge--error to true based on type", () => {
+      expect(component.badgeStyles["go-pill-badge--error"]).toBeFalsy();
+
+      component.type = "error";
+      component.ngOnChanges();
+      expect(component.badgeStyles["go-pill-badge--error"]).toBe(true);
+    });
+
+    it("returns an object that set go-pill-badge--info to true based on type", () => {
+      expect(component.badgeStyles["go-pill-badge--info"]).toBeFalsy();
+
+      component.type = "info";
+      component.ngOnChanges();
+      expect(component.badgeStyles["go-pill-badge--info"]).toBe(true);
+    });
   });
 });
