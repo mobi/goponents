27c03b22f4fc26d8de3a8b3f168ae9f5eda29b72
Vishal Shah <vishal.acharya@tangoe.com>
2023-05-22 11:39:59 +0530

    bug fixes for select all filtered list

--- projects/go-lib/src/lib/components/go-select/go-select.component.ts
+++ projects/go-lib/src/lib/components/go-select/go-select.component.ts
@@ -1,14 +1,30 @@
-import { Component, ContentChild, EventEmitter, Input, OnInit, Output, TemplateRef, ViewEncapsulation } from '@angular/core';
-import { Subject } from 'rxjs';
-import { GoFormBaseComponent } from '../go-form-base/go-form-base.component';
+import {
+  Component,
+  ContentChild,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  TemplateRef,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
+import { Subject, Subscription } from "rxjs";
+import { GoFormBaseComponent } from "../go-form-base/go-form-base.component";
+import { NgSelectComponent } from "@ng-select/ng-select";
 
 @Component({
   encapsulation: ViewEncapsulation.None,
-  selector: 'go-select',
-  templateUrl: './go-select.component.html',
-  styleUrls: ['./go-select.component.scss']
+  selector: "go-select",
+  templateUrl: "./go-select.component.html",
+  styleUrls: ["./go-select.component.scss"],
 })
-export class GoSelectComponent extends GoFormBaseComponent implements OnInit {
+export class GoSelectComponent
+  extends GoFormBaseComponent
+  implements OnInit, OnDestroy
+{
+  @ViewChild(NgSelectComponent) ngSelect: NgSelectComponent;
 
   @Input() appendTo: string;
   @Input() bindLabel: string;
@@ -30,26 +46,84 @@ export class GoSelectComponent extends GoFormBaseComponent implements OnInit {
   @Input() searchable: boolean = true;
   @Input() showSelectAll: boolean = true;
   @Input() typeahead?: Subject<string>;
-  @Input() typeToSearchText: string = 'Type to Search';
+  @Input() typeToSearchText: string = "Type to Search";
   @Input() virtualScroll: boolean = false;
 
   @Output() scrollToEnd: EventEmitter<any> = new EventEmitter<any>();
-  @Output() scroll: EventEmitter<{ start: number, end: number }> = new EventEmitter<{ start: number; end: number }>();
+  @Output() scroll: EventEmitter<{ start: number; end: number }> =
+    new EventEmitter<{ start: number; end: number }>();
 
-  @ContentChild('goSelectOption') goSelectOption: TemplateRef<any>;
-  @ContentChild('goSelectOptionGroup') goSelectOptionGroup: TemplateRef<any>;
-  @ContentChild('goSelectSelectedOption') goSelectSelectedOption: TemplateRef<any>;
+  @ContentChild("goSelectOption") goSelectOption: TemplateRef<any>;
+  @ContentChild("goSelectOptionGroup") goSelectOptionGroup: TemplateRef<any>;
+  @ContentChild("goSelectSelectedOption")
+  goSelectSelectedOption: TemplateRef<any>;
+
+  private controlSubscription: Subscription;
+  // store refined items after search
+  private refinedItems: any[] = [];
+  // stores previous selected items when typeahead is enabled only in case of selectAll.
+  private previousSelectedItems: any[] = [];
 
   ngOnInit(): void {
     this.closeOnSelect = this.multiple ? false : this.closeOnSelect;
+    this.controlSubscription = this.control.valueChanges.subscribe((value) => {
+      if (this.multiple && this.showSelectAll) {
+        this.emptyRefinedItems();
+        if (!value?.length) {
+          this.resetTypeaheadItems();
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.controlSubscription.unsubscribe();
   }
 
   onSelectAll(): void {
-    this.control.patchValue(this.items.map((item: any) => this.bindValue ? item[this.bindValue] : item));
+    if (this.typeahead) {
+      this.handleTypeaheadSelectAll();
+      return;
+    }
+    const items = this.refinedItems.length ? this.refinedItems : this.items;
+    this.processSelectAll(items);
+  }
+
+  private processSelectAll(items: any[]) {
+    const refinedArr = items.map((item: any) =>
+      this.bindValue ? item[this.bindValue] : item
+    );
+    const existing = Array.isArray(this.control.value) ? this.control.value : [];
+    this.control.patchValue(existing.concat(refinedArr));
+    this.ngSelect.searchTerm = "";
+    this.ngSelect.itemsList.resetFilteredItems();
+  }
+
+  private handleTypeaheadSelectAll() {
+    // because spread operator is not supported due to tslib version
+    const items = JSON.parse(JSON.stringify(this.items));
+    for (let previousItem of this.previousSelectedItems) {
+      const exists = items.find(
+        (item) => item[this.bindValue] === previousItem[this.bindValue]
+      );
+      if (!exists) {
+        items.unshift(previousItem);
+      }
+    }
+
+    this.previousSelectedItems = items;
+    this.items = items;
+    this.control.reset([], { emitEvent: false });
+    this.processSelectAll(items);
+  }
+
+  handleInput(search: { term: string; items: any[] }) {
+    this.refinedItems = search.items;
   }
 
   onRemoveAll(): void {
     this.control.reset();
+    this.resetTypeaheadItems();
   }
 
   onScrollToEnd(): void {
@@ -60,5 +134,20 @@ export class GoSelectComponent extends GoFormBaseComponent implements OnInit {
 
   onScroll($event: { start: number; end: number }): void {
     this.scroll.emit($event);
+  }
+
+  private resetTypeaheadItems() {
+    if (this.typeahead) {
+      this.items = [];
+      this.previousSelectedItems = [];
+    }
+  }
+
+  private emptyRefinedItems(): void {
+    this.refinedItems = [];
+  }
+
+  onClose() {
+    this.emptyRefinedItems();
   }
 }
