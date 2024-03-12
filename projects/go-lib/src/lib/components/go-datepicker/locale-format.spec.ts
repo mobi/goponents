18cadbb3e17cb778a2daa8e047e8368746f50446
Preeti.Gairi <preeti.gairi@Tangoe.com>
2024-03-12 18:43:50 +0530

    #GOCLD 2298 test cases

--- projects/go-lib/src/lib/components/go-datepicker/locale-format.spec.ts
+++ projects/go-lib/src/lib/components/go-datepicker/locale-format.spec.ts
@@ -13,16 +13,14 @@ describe('LocaleFormat', () => {
         dec.toUTCString() + ' is not a valid date'
       );
     });
-    it('should return false for a non leap year date', () => {
-      const nonLeapYear: Date = new Date(2021, 1, 29);
-      expect(LocaleFormat.validDate(nonLeapYear.getMonth(), nonLeapYear.getDate(), nonLeapYear.getFullYear())).toBeTruthy(
-        nonLeapYear.toUTCString() + ' is not a valid date'
-      )});
+    it('should return true for a leap year date', () => {
+      const feb = 1;
+      expect(LocaleFormat.validDate(feb, 29, 2024)).toBe(true);
+    });
 
-    it('should return true for a  leap year date', () => {
-      const leapYear: Date = new Date(2020, 1, 29);
-      expect(LocaleFormat.validDate(leapYear.getMonth(), leapYear.getDate(), leapYear.getFullYear())).toBeTruthy(
-        leapYear.toUTCString() + ' is a valid date'
-      )});
+    it('should return false for a non leap year date', () => {
+      const feb = 1;
+      expect(LocaleFormat.validDate(feb, 29, 2023)).toBe(false);
+    });
   });
 });
